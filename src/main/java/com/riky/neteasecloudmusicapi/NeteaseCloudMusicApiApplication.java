37a53f336bbcdc54057d5b34f330d5e58e5017a4
RikyLi <RikyLi@viatech.com.cn>
2018-05-17 18:06:57 +0800

    init

---
+++ src/main/java/com/riky/neteasecloudmusicapi/NeteaseCloudMusicApiApplication.java
@@ -0,0 +1,12 @@
+package com.riky.neteasecloudmusicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeteaseCloudMusicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeteaseCloudMusicApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/riky/neteasecloudmusicapi/NeteaseCloudMusicApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.riky.neteasecloudmusicapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeteaseCloudMusicApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
